eafb8ce210b1c7814f3f8df049570ed4b40435b1
kossyak <akosyak@protonmail.com>
2022-12-07 12:55:45 +0400

    first

---
+++ scripts/index.js
@@ -0,0 +1,24 @@
+import Eden from '../eden.js'
+
+const container = document.getElementById('edenContainer')
+const eden = new Eden(container)
+
+eden.render(10, 8)
+
+eden.build('wall', {
+  coords: [{y: 2, x: 1}, {y: 2, x: 0}],
+  color: 'gray'
+})
+
+eden.listener('left', () => console.log('click left'))
+
+eden.spawn('tiger', {
+  y: 5,
+  x: 0,
+  color: 'orange'
+})
+eden.spawn('meet', {
+  y: eden.random(9),
+  x: eden.random(9),
+  color: 'red'
+})
